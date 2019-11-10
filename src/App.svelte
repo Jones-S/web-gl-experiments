7d32136d64518b77971a1c95d46f155d52423bfa
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-11-10 21:06:11 +0100

    [Init] Init simple svelte app and create blotter fluid text effect

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import TypoDistortion from './components/TypoDistortion/TypoDistortion.svelte'
+</script>
+
+<style lang='sass'>
+	@import './css/main.scss';
+</style>
+
+<TypoDistortion />
+
